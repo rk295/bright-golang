471160c462c31f248a8c28cc79ac07b8b25e1cb8
Robin Kearney <robin@kearney.co.uk>
2021-12-28 07:02:02 +0000

    tidying docs

--- doc.go
+++ doc.go
@@ -1,17 +1,8 @@
 /*
 Package bright-golang is a client for the Bright App energy monitoring API.
 
-Not all the API endpoints are implemented, however there should be sufficient
-to at least get Electricity and Gas usage out. There are a few helper functions
-which attempt to figure out which resource holds the current usage (in Watts) for both
-gas and electricity, they are the simplest ways of using the package.
-
-A username and password for the API is required, you can either pass those into
-NewClient() or you can have them read from the environment by calling
-NewClientFromEnv().  This uses the BRIGHT_USERNAME and BRIGHT_PASSWORD
-environment variables.
-
-To quickly see your current electricity usage in Watts you could do somehting like:
+After setting BRIGHT_USERNAME and BRIGHT_PASSWORD you could quickly see your
+current electricity usage in Watts by doing:
 
 	package main
 	import (
