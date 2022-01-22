ec71f71d12c13e5a6eab134c5ce9a8bf09a87dd4
Robin Kearney <robin@kearney.co.uk>
2022-01-22 22:36:48 +0000

    docs for sub package

--- mqtt/doc.go
+++ mqtt/doc.go
@@ -4,10 +4,10 @@ package mqtt
 Package bright-golang/mqtt is a set of types which aide in the Unmarshalling
 of the JSON messages received via MQTT.
 
-Given a MQTT message payload in m the following will print the current
+Given a MQTT message payload  m the following will print the current
 Electricity usage in Watts
 
-	if err := json.Unmarshal(m.Payload(), &decoded); err != nil {
+	if err := json.Unmarshal(m, &decoded); err != nil {
 		fmt.Println("error", err)
 	}
 	fmt.Println(decoded.ElecMtr.Metering.HistoricalConsumption.InstantaneousDemand)
